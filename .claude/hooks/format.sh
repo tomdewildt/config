af202cb45014439e85eb453b6286f5bcfd8b787c
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2026-02-26 21:45:09 +0100

    Add claude code format hook (#151)

---
+++ .claude/hooks/format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# Select fields
+INPUT=$(cat)
+FILE_PATH=$(echo "$INPUT" | jq -r '.tool_input.file_path')
+
+# Run format
+if [[ "$FILE_PATH" == *.go ]]; then
+    go fmt "$FILE_PATH" &> /dev/null
+elif [[ "$FILE_PATH" == *.css || "$FILE_PATH" == *.html || "$FILE_PATH" == *.js || "$FILE_PATH" == *.json || "$FILE_PATH" == *.jsx || "$FILE_PATH" == *.ts  || "$FILE_PATH" == *.tsx ]]; then
+    npx -y prettier --write "$FILE_PATH" &> /dev/null
+elif [[ "$FILE_PATH" == *.py ]]; then
+    ruff format "$FILE_PATH" &> /dev/null
+elif [[ "$FILE_PATH" == *.tf || "$FILE_PATH" == *.tfvars ]]; then
+    terraform fmt "$FILE_PATH" &> /dev/null
+fi
